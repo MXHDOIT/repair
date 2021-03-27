13e63cb3a3606963a75ad197a12bc4b4f9a5e9e1
maxinhang <maxinhangdoit@163.com>
2021-03-27 10:41:25 +0800

    project init

---
+++ src/main/java/com/xpu/RepairApplication.java
@@ -0,0 +1,13 @@
+package com.xpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairApplication.class, args);
+    }
+
+}
